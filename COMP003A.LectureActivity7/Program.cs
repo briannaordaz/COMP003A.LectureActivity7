949717ecd80a37f73436b9673574c71c39f5777c
briannaordaz <briannaordaz5@gmail.com>
2026-02-25 21:28:15 -0800

    Completed part A

---
+++ COMP003A.LectureActivity7/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 5, 10, 15, 20, 25 };
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }
+    }
+}
